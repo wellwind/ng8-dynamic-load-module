0b051f36c9b1b9ef6dc57e78eebef9fa31f08c70
wellwind <wellwind@mail2000.com.tw>
2019-06-04 21:33:26 +0800

    動態載入元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,22 @@
-import { Compiler, Component, Injector, OnInit } from '@angular/core';
+import { Compiler, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(private compiler: Compiler, private injector: Injector) {}
+export class AppComponent {
+  lazyModuleFactory: any;
+  lazyComponent: any;
 
-  ngOnInit() {
+  constructor(private compiler: Compiler) {}
+
+  loadComponent() {
     import('./lazy-page/lazy-page.module').then(loadedModule => {
       const lazyPageModule = loadedModule.LazyPageModule;
       this.compiler.compileModuleAsync(lazyPageModule).then(moduleFactory => {
-        const moduleRef = moduleFactory.create(this.injector);
-        console.log(moduleRef);
+        this.lazyModuleFactory = moduleFactory;
+        this.lazyComponent = lazyPageModule.EntryComponent;
       });
     });
   }
--- src/app/lazy-page/lazy-page.module.ts
+++ src/app/lazy-page/lazy-page.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { LazyComponentComponent } from './lazy-component/lazy-component.component';
 
 @NgModule({
   declarations: [LazyComponentComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  entryComponents: [LazyComponentComponent]
 })
-export class LazyPageModule { }
+export class LazyPageModule {
+  static EntryComponent = LazyComponentComponent;
+}
