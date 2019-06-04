0d53c2defe4e92550909ef8a78d78d92a9ae0f90
wellwind <wellwind@mail2000.com.tw>
2019-06-04 21:16:21 +0800

    動態載入並建立模組

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Compiler, Component, Injector, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng8-dynamic-load-module';
+export class AppComponent implements OnInit {
+  constructor(private compiler: Compiler, private injector: Injector) {}
+
+  ngOnInit() {
+    import('./lazy-page/lazy-page.module').then(loadedModule => {
+      const lazyPageModule = loadedModule.LazyPageModule;
+      this.compiler.compileModuleAsync(lazyPageModule).then(moduleFactory => {
+        const moduleRef = moduleFactory.create(this.injector);
+        console.log(moduleRef);
+      });
+    });
+  }
 }
